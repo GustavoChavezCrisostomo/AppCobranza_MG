cb3fd9caf3d388a37b8ab78866e3ce493e7a9bf7
Gustavo Chavez <gustavo.chavez.c@tecsup.edu.pe>
2018-11-21 14:34:53 -0500

    avance Login

--- app/src/main/java/com/tesis/gchavez/appcobranza/activity/PrincipalActivity.java
+++ app/src/main/java/com/tesis/gchavez/appcobranza/activity/PrincipalActivity.java
@@ -1,13 +1,15 @@
-package com.tesis.gchavez.appcobranza;
+package com.tesis.gchavez.appcobranza.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tesis.gchavez.appcobranza.R;
+
+public class PrincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_principal);
     }
 }
---
+++ app/src/main/java/com/tesis/gchavez/appcobranza/models/Usuario.java
@@ -0,0 +1,20 @@
+package com.tesis.gchavez.appcobranza.models;
+
+public class Usuario {
+
+    private Integer id;
+    private String login;
+    private String password;
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private String zona;
+    private Integer clienAsig;
+    private String role;
+
+    public Usuario(){
+    }
+
+
+
+}
